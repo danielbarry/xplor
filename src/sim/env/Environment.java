1b400f6c3328894a95a01989a3c7295ab64a2bc9
Dan <danbarry16@googlemail.com>
2016-05-07 20:30:59 +0100

    Simple definition of Environment

---
+++ src/sim/env/Environment.java
@@ -0,0 +1,11 @@
+package joint.xplor.sim.env;
+
+/**
+ * Environment.java
+ *
+ * This is a simple interface that defines how an environment will be
+ * interacted with.
+ **/
+public interface Environment{
+  /* TODO: Write this code. */
+}
