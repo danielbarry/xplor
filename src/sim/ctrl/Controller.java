876ecd4dfc59363c3f0599777523baeba3618bf2
Dan <danbarry16@googlemail.com>
2016-05-07 20:30:43 +0100

    Simple definition of Controller

---
+++ src/sim/ctrl/Controller.java
@@ -0,0 +1,11 @@
+package joint.xplor.sim.ctrl;
+
+/**
+ * Controller.java
+ *
+ * This is a simple interface that defines how an agent will be interacted
+ * with.
+ **/
+public interface Controller{
+  /* TODO: Write this code. */
+}
