43785c54dd9f9fa130800eb1cb37791cdaee89d7
Dan <danbarry16@googlemail.com>
2016-05-07 19:56:51 +0100

    Basic entry class for project

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+package joint.xplor;
+
+/**
+ * Main.java
+ *
+ * Entry class into the main system. The responsibility of this class is to
+ * handle all input from the command line if any and start the program
+ * appropriately.
+ **/
+public class Main{
+  /**
+   * main()
+   *
+   * The main entry method into the program. This method is expected to parse
+   * the command line inputs, handle the programs safety and begin the main
+   * execution of the program.
+   *
+   * @param args The arguments given to this program from the command line.
+   **/
+  public static void main(String[] args){
+    /* TODO: Write this section. */
+    System.out.println("Program was started.");
+  }
+}
