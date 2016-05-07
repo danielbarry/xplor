6cd9219237aaecc4b9aa2e33a52e65351f1be3e1
Dan <danbarry16@googlemail.com>
2016-05-07 20:31:31 +0100

    Simple GUI class

---
+++ src/gui/GUI.java
@@ -0,0 +1,30 @@
+package joint.xplor.gui;
+
+import joint.xplor.sim.Simulation;
+
+/**
+ * GUI.java
+ *
+ * This class represents the GUI that will always appear for main project,
+ * offering core features to the main program. The core approach to this class
+ * is that it should be small, light and effective. Only core, non-specialised
+ * code will be supported here.
+ *
+ * From a structural perspective, this class serves as a wrapper for the system
+ * core functionality rather than being the system's core functionality. The
+ * simulation should run effectively if this class was to be removed.
+ **/
+public class GUI{
+  /**
+   * GUI()
+   *
+   * This method is responsible for initialising the GUI class that wraps
+   * around and serves the simulation.
+   *
+   * @param sim The instance of the simulation to be wrapped by this GUI class.
+   * This value must not be NULL.
+   **/
+  public GUI(Simulation sim){
+    /* TODO: Write this code. */
+  }
+}
