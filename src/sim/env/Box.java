9f259ef63e21d71ecb969b6be041fdbf43112b9d
Dan <danbarry16@googlemail.com>
2016-05-09 17:50:29 +0100

    Basic implementation of environment

---
+++ src/sim/env/Box.java
@@ -0,0 +1,11 @@
+package joint.xplor.sim.env;
+
+/**
+ * Box.java
+ *
+ * A simple box shaped implementation of an environment to test basic movement
+ * and collision detection.
+ **/
+public class Box implements Environment{
+  /* TODO: Implement environment methods when written. */
+}
