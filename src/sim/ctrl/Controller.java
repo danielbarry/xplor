c84552c699dd39320db30b6ab0b8a39abcda1f5b
Dan <danbarry16@googlemail.com>
2016-05-09 17:50:46 +0100

    Commenting for controller interface

--- src/sim/ctrl/Controller.java
+++ src/sim/ctrl/Controller.java
@@ -7,6 +7,26 @@ package joint.xplor.sim.ctrl;
  * with.
  **/
 public interface Controller{
+  /**
+   * getMove()
+   *
+   * Gets the next agent move for the given raw input.
+   *
+   * NOTE: This method puts direct delay into the number of updates the program
+   *       is capable of running. Heavy code here may significantly slow down
+   *       the system.
+   *
+   * @param input The raw sensory input for the environment.
+   * @return The move in which the agent decides to use.
+   **/
   public Move getMove(byte[] input);
+
+  /**
+   * getKnowledge()
+   *
+   * Gets the agent's knowledge in raw format.
+   *
+   * @return The raw knowledge of length defined by the agent.
+   **/
   public byte[] getKnowledge();
 }
